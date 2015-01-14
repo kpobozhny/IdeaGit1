24ce7c6823e187a5c8e93731bbb325bb9db16e6b
kostya <kosty@test.com>
2015-01-14 16:05:00 +0200

    first commit

---
+++ src/com/kok/pack1/net1.java
@@ -0,0 +1,32 @@
+package com.kok.pack1;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by kostya on 1/14/15.
+ */
+public class net1 {
+    /// changes
+
+    public static void main(String[] args)  {
+
+        try {
+            InetAddress Address = InetAddress.getLocalHost();
+            System.out.println(Address);
+
+            Address = InetAddress.getByName("www.google.com");
+            System.out.println(Address);
+
+            InetAddress[] SW = InetAddress.getAllByName("www.luxoft.com");
+            for (int i=0; i<SW.length;i++)
+                System.out.println(SW[i]);
+
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}

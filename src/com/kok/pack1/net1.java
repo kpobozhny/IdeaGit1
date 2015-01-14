e2b4fddf90c02c5c53e920b8d8c9b712adf5391f
kostya <kosty@test.com>
2015-01-14 16:16:49 +0200

    set f = 12 in net1.java;

--- src/com/kok/pack1/net1.java
+++ src/com/kok/pack1/net1.java
@@ -13,7 +13,7 @@ public class net1 {
 
         try {
             int r = 5;
-            int f = 6;
+            int f = 12;
             InetAddress Address = InetAddress.getLocalHost();
             System.out.println(Address);
 
